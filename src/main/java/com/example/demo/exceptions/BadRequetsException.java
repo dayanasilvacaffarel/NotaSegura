e9a025b3e68001a380ddab4c8ba710617ad48f0c
DayanaSilvaSolcre <dayana.silva@solcre.com>
2023-11-10 13:17:13 -0300

    <Nota Segura> Repository

---
+++ src/main/java/com/example/demo/exceptions/BadRequetsException.java
@@ -0,0 +1,5 @@
+package com.example.demo.exceptions;
+
+public class BadRequetsException extends Exception{
+    public BadRequetsException(String mensaje) { super(mensaje);}
+}
---
+++ src/main/java/com/example/demo/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+package com.example.demo.exceptions;
+
+public class ResourceNotFoundException extends Exception{
+    public ResourceNotFoundException(String mensaje){super (mensaje);}
+
+}
